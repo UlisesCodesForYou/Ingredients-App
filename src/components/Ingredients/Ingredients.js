1cc2136b7cde06765d6bca78789584a623ca7309
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-06-18 12:45:52 -0700

    Able to post data to google firebase.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -7,11 +7,34 @@ import IngredientList from "./IngredientList";
 const Ingredients = () => {
   const [userIngredients, setIngredients] = useState([]);
 
+  useEffect(() => {}, [])
+
   const addIngredientHandler = (ingredient) => {
-    setIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(
+      "https://ingredient-app-5f610-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (id) => {
+    const removedFromList = userIngredients.filter((item) => item.id !== id);
+
+    setIngredients(removedFromList);
   };
 
   return (
@@ -20,7 +43,10 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
