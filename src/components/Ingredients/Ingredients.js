42c35d88c020a87a057e2eb4ec182d71b302ea10
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-06-22 20:47:49 -0700

    Added useReducer to clean code up.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,18 +1,38 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useReducer } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 
+const ingredientReducer = (currentIngredient, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredient, action.ingredient];
+    case "DELETE":
+      return currentIngredient.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error(
+        "If you are reading this, then something went really wrong"
+      );
+  }
+};
+
 const Ingredients = () => {
-  const [userIngredients, setIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, [
+    // currentIngredient,
+  ]);
+  // const [userIngredients, setIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setIsError] = useState();
+
   // #############  This useEffect is loading data from firebase database.
 
   const filteredIngredients = useCallback((filteredIngredients) => {
-    setIngredients(filteredIngredients);
+    // setIngredients(filteredIngredients);
+    dispatch({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
   //  ############# I am posting data to firebase using the fetch method.
@@ -33,10 +53,14 @@ const Ingredients = () => {
         return response.json();
       })
       .then((responseData) => {
-        setIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient },
-        ]);
+        // setIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient },
+        // ]);
+        dispatch({
+          type: "ADD",
+          ingredient: { id: responseData.name, ...ingredient },
+        });
       });
   };
 
@@ -51,11 +75,11 @@ const Ingredients = () => {
     )
       .then((response) => {
         setIsLoading(false);
-        const removedFromList = userIngredients.filter(
-          (item) => item.id !== ingredientId
-        ); // Changed id to ingredientId
-
-        setIngredients(removedFromList);
+        // const removedFromList = userIngredients.filter(
+        //   (item) => item.id !== ingredientId
+        // ); // Changed id to ingredientId
+        dispatch({ type: "DELETE", id: ingredientId });
+        // setIngredients(removedFromList);
       })
       .catch((error) => {
         setIsError(error.message);
@@ -65,7 +89,6 @@ const Ingredients = () => {
 
   const clearError = () => {
     setIsError(null);
-    
   };
 
   return (
